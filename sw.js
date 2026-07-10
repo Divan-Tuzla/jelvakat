189e0cdd62fc3875d16ec416600c0805a2b8be1f
Divan-Tuzla <udruzenjedivantuzla@gmail.com>
2026-07-10 20:11:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 // Verzija keša se mijenja sa svakom novom verzijom app-a. Stari keš se briše.
 // ════════════════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'jv-v3.2.0';
+const CACHE_VERSION = 'jv-v3.3.0';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
